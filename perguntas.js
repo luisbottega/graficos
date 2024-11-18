90c7f2908d41d4232b0b8d40a52aa0ac2df61f75
luis otavio <105716836+luisbottega@users.noreply.github.com>
2024-11-18 10:16:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+  "Fertilidade",
+  "Como a fertilização pode impactar a qualidade do grão de trigo?",
+  "A fertilização adequada pode aumentar o teor de proteína e o peso específico dos grãos, melhorando sua qualidade para processamento.."
+);
+
+criaCartao(
+  "Manejo do solo",
+  "Quais práticas ajudam na conservação do solo?",
+  "Práticas como plantio direto, cobertura do solo, rotação de culturas e terraceamento ajudam na conservação do solo."
+);
+
+criaCartao(
+  "materia organico no solo",
+  "Qual a importância da matéria orgânica no solo?",
+  "A matéria orgânica melhora a estrutura do solo, aumentaa capacidade de retenção de água e nutrientes, e favorece a atividade biológica."
+);
+
+criaCartao(
+  "nutrientes que o milho precisa",
+  "Quais são os principais nutrientes que o milho necessita?",
+  "Os principais nutrientes são nitrogênio (N), fósforo (P) e potássio (K). O milho também se beneficia de micronutrientes como zinco e manganês."
+);
+
+criaCartao(
+  "rotaçao e cultura",
+  "Como a rotação de culturas beneficia o solo?",
+  "A rotação de culturas melhora a fertilidade do solo, reduz a incidência de pragas e doenças, e evita o esgotamento de nutrientes"
+);
+
+criaCartao(
+  "cultivo de trigo",
+  "Qual o efeito da compactação do solo no cultivo de trigo?",
+  "A compactação do solo reduz a aeração e a infiltração de água, prejudicando o desenvolvimento das raízes e a absorção de nutrientes."
+);
+
+criaCartao(
+  "proutiviade o soja",
+  "Quais são os principais fatores que influenciam a produtividade da soja?",
+  "Fatores como a qualidade do solo, clima, manejo da irrigação, controle de pragas e doenças, e a escolha da variedade influenciam a produtividade."
+);
+
+criaCartao(
+  "acidez do solo",
+  "Como a acidez do solo afeta o crescimento das plantas?",
+  "A acidez excessiva pode dificultar a absorção de nutrientes e toxinas, como alumínio, prejudicando o crescimento das plantas."
+);
+
+criaCartao(
+  "plantiu do milho",
+  "Qual é a melhor época para plantar milho em climas temperados?",
+  "A melhor época é geralmente na primavera, quando as temperaturas do solo atingem cerca de 10-12°C"
+);
+
+criaCartao(
+  "Agrotoxicos",
+  " O que são agrotóxicos e qual é a sua função?",
+  "Agrotóxicos são substâncias químicas usadas na agricultura para controlar pragas, doenças e plantas daninhas. Eles ajudam a aumentar a produtividade das culturas, protegendo as plantações e garantindo uma colheita mais saudável."
+);
+
+criaCartao(
+  "agricultura",
+  " Quais são os principais tipos de agricultura?",
+  "Os principais tipos de agricultura incluem a agricultura convencional, que utiliza fertilizantes e agrotóxicos químicos; a agricultura orgânica, que evita esses produtos e se baseia em práticas sustentáveis; e a agricultura de precisão, que utiliza tecnologias para otimizar o uso de recursos e aumentar a eficiência das colheitas."
+);
+
+criaCartao(
+  "agricultura",
+  " Como a agricultura pode impactar o meio ambiente?",
+  "A agricultura pode impactar o meio ambiente de várias maneiras, como a degradação do solo devido ao uso excessivo de produtos químicos, a contaminação da água por escoamento de fertilizantes, e a perda de biodiversidade pela conversão de habitats naturais em áreas cultiváveis. Práticas agrícolas sustentáveis visam mitigar esses impactos."
+);
